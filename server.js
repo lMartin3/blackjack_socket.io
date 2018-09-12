4ab7b65fb00adea14d7ed08611681225daa22f57
Martín <martinlufr@gmail.com>
2018-09-12 18:53:41 -0300

    Readme changes

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ var io = socketIO(server);
 app.set('port', 3000);
 app.use('/static', express.static(__dirname + '/static'));
 app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+    app.use(express.static(path.join(__dirname, 'static')));
+  });
+  // Starts the server.
+  server.listen(3000, function() {
+    console.log('Starting server on port 3000');
+  });
+  io.on('connection', function(socket) {
+    console.log("new connection from " + socket.ip);
+  });

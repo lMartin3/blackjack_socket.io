5f7df5db64f5fa2f2e30521e4a1d8fc4d6b234b1
Martín <martinlufr@gmail.com>
2018-09-12 23:12:57 -0300

    HTML files commit

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ const socketIO = require('socket.io');
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
-app.set('port', 3000);
+/* app.set('port', 3000);
 app.use('/static', express.static(__dirname + '/static'));
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'static'))); */
 
 app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, 'index.html'));
+    response.sendFile(path.join(__dirname, '/static/', 'index.html'));
     app.use(express.static(path.join(__dirname, 'static')));
-  });
-  // Starts the server.
-  server.listen(3000, function() {
+});
+// Starts the server.
+server.listen(3000, function() {
     console.log('Starting server on port 3000');
-  });
-  io.on('connection', function(socket) {
+});
+io.on('connection', function(socket) {
     console.log("new connection from " + socket.ip);
-  });
+});
---
+++ static/game.js
@@ -0,0 +1,4 @@
+$(function(){
+    var socket = io.connect();
+    alert("Script loaded!");
+})
